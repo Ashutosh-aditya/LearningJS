e980e9c1718f4a1f3490d0593e9de2c6a359e16c
Ashutosh Aditya <adityaashutosh06@gmail.com>
2023-04-16 13:47:47 +0530

    .

--- node_js/using_PATH_module1.js
+++ node_js/using_PATH_module1.js
@@ -2,3 +2,10 @@
 const path = require('path');
 console.log(path.sep);
 console.log(path.join('fd1','fd2','fd3','fd4'));
+const str=path.join('fd1','fd2','fd3','fd4');
+console.log(path.resolve(__dirname,str));
+console.log(path.resolve(__dirname));
+console.log(path.resolve(__filename));
+
+
+
